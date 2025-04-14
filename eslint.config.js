b0c2b371cf7ebfbbcd0c3917a835091dc3bc06e9
puskar789 <pbohara231159@gmail.com>
2025-04-14 19:12:45 +0530

    changed the eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -20,6 +20,7 @@ export default tseslint.config(
     rules: {
       "@typescript-eslint/array-type": "off",
       "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
   {
