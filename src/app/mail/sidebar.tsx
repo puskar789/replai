4ea93d4d5511b9a7ac9b5699b3ca0a7387f65b08
puskar789 <pbohara231159@gmail.com>
2025-06-04 13:18:59 +0530

    completed the middle sidebar portion

--- src/app/mail/sidebar.tsx
+++ src/app/mail/sidebar.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { useLocalStorage } from "usehooks-ts";
 import { Nav } from "./nav";
 import { File, Inbox, Send } from "lucide-react";
+import { api } from "@/trpc/react";
 
 type Props = {
   isCollapsed: boolean;
@@ -16,25 +17,38 @@ const Sidebar = ({ isCollapsed }: Props) => {
     "inbox",
   );
 
+  const { data: inboxThreads } = api.account.getNumThreads.useQuery({
+    accountId,
+    tab: "inbox",
+  });
+  const { data: draftThreads } = api.account.getNumThreads.useQuery({
+    accountId,
+    tab: "draft",
+  });
+  const { data: sentThreads } = api.account.getNumThreads.useQuery({
+    accountId,
+    tab: "sent",
+  });
+
   return (
     <Nav
       isCollapsed={isCollapsed}
       links={[
         {
           title: "Inbox",
-          label: "1",
+          label: inboxThreads?.toString() ?? "0",
           icon: Inbox,
           variant: tab === "inbox" ? "default" : "ghost",
         },
         {
           title: "Draft",
-          label: "4",
+          label: draftThreads?.toString() ?? "0",
           icon: File,
           variant: tab === "draft" ? "default" : "ghost",
         },
         {
           title: "Sent",
-          label: "6",
+          label: sentThreads?.toString() ?? "0",
           icon: Send,
           variant: tab === "sent" ? "default" : "ghost",
         },
--- src/server/api/routers/account.ts
+++ src/server/api/routers/account.ts
@@ -1,4 +1,29 @@
+import { z } from "zod";
 import { createTRPCRouter, privateProcedure } from "../trpc";
+import { db } from "@/server/db";
+
+export const authoriseAccountAccess = async (
+  accountId: string,
+  userId: string,
+) => {
+  const account = await db.account.findFirst({
+    where: {
+      id: accountId,
+      userId: userId,
+    },
+    select: {
+      id: true,
+      emailAddress: true,
+      token: true,
+    },
+  });
+
+  if (!account) {
+    throw new Error("Account not found.");
+  }
+
+  return account;
+};
 
 export const accountRouter = createTRPCRouter({
   getAccounts: privateProcedure.query(async ({ ctx }) => {
@@ -13,4 +38,26 @@ export const accountRouter = createTRPCRouter({
       },
     });
   }),
+  getNumThreads: privateProcedure
+    .input(
+      z.object({
+        accountId: z.string(),
+        tab: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      const account = await authoriseAccountAccess(
+        input.accountId,
+        ctx.auth.userId,
+      );
+
+      return await ctx.db.thread.count({
+        where: {
+          accountId: account.id,
+          inboxStatus: input.tab === "inbox" ? true : false,
+          draftStatus: input.tab === "draft" ? true : false,
+          sentStatus: input.tab === "sent" ? true : false,
+        },
+      });
+    }),
 });
